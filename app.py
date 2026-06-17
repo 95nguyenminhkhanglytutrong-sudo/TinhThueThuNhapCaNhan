b5389b86fc2465ed4be2380362cae331a0a263fc
95nguyenminhkhanglytutrong-sudo <95nguyenminhkhang.lytutrong@gmail.com>
2026-06-17 17:57:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.image("IMG_6477.JPG")
+st.image("z7946906147278_ee061fe7efc447c6c9f3a4eaebb0656a.jpg")
 st.title("💰 App tính Thuế Thu Nhập Cá Nhân đề tài 6 Nguyễn Minh Khang")
 
 # Nhập dữ liệu
