518e0d76b29bc3929224aee6d912eb4217d5a5c0
95nguyenminhkhanglytutrong-sudo <95nguyenminhkhang.lytutrong@gmail.com>
2026-06-17 17:42:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,48 +15,48 @@ if st.button("Tính thuế"):
 
     tax = 0
 
-    # Tính thuế theo biểu thuế lũy tiến từng phần
-    if thu_nhap <= 5:
-        tax = thu_nhap * 0.05
+   # Tính thuế theo biểu thuế mới
+if thu_nhap <= 5:
+    tax = thu_nhap * 0.02
 
-    elif thu_nhap <= 10:
-        tax = 5 * 0.05 + (thu_nhap - 5) * 0.10
+elif thu_nhap <= 10:
+    tax = 5 * 0.02 + (thu_nhap - 5) * 0.04
 
-    elif thu_nhap <= 18:
-        tax = 5 * 0.05 + 5 * 0.10 + (thu_nhap - 10) * 0.15
+elif thu_nhap <= 18:
+    tax = 5 * 0.02 + 5 * 0.04 + (thu_nhap - 10) * 0.06
 
-    elif thu_nhap <= 32:
-        tax = 5 * 0.05 + 5 * 0.10 + 8 * 0.15 + (thu_nhap - 18) * 0.20
+elif thu_nhap <= 32:
+    tax = 5 * 0.02 + 5 * 0.04 + 8 * 0.06 + (thu_nhap - 18) * 0.08
 
-    elif thu_nhap <= 52:
-        tax = (
-            5 * 0.05 +
-            5 * 0.10 +
-            8 * 0.15 +
-            14 * 0.20 +
-            (thu_nhap - 32) * 0.25
-        )
+elif thu_nhap <= 52:
+    tax = (
+        5 * 0.02 +
+        5 * 0.04 +
+        8 * 0.06 +
+        14 * 0.08 +
+        (thu_nhap - 32) * 0.10
+    )
 
-    elif thu_nhap <= 80:
-        tax = (
-            5 * 0.05 +
-            5 * 0.10 +
-            8 * 0.15 +
-            14 * 0.20 +
-            20 * 0.25 +
-            (thu_nhap - 52) * 0.30
-        )
+elif thu_nhap <= 80:
+    tax = (
+        5 * 0.02 +
+        5 * 0.04 +
+        8 * 0.06 +
+        14 * 0.08 +
+        20 * 0.10 +
+        (thu_nhap - 52) * 0.12
+    )
 
-    else:
-        tax = (
-            5 * 0.05 +
-            5 * 0.10 +
-            8 * 0.15 +
-            14 * 0.20 +
-            20 * 0.25 +
-            28 * 0.30 +
-            (thu_nhap - 80) * 0.35
-        )
+else:
+    tax = (
+        5 * 0.02 +
+        5 * 0.04 +
+        8 * 0.06 +
+        14 * 0.08 +
+        20 * 0.10 +
+        28 * 0.12 +
+        (thu_nhap - 80) * 0.15
+    )
 
     thu_nhap_sau_thue = thu_nhap - tax
 
